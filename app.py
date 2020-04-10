c793259c9b517db32c17e0cf5c12e5fb1270f9a0
Damien Milon <milon.damien@gmail.com>
2020-04-10 14:27:49 +0200

    Bind port

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import os
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 api = Api(app)
+port = int(os.environ.get("PORT", 5000))
+
 @app.before_first_request
 def create_tables():
   db.create_all()
@@ -16,4 +18,4 @@ api.add_resource(RestaurantMember, "/restaurants/<id>")
 
 db.init_app(app)
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=port)
