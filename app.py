0367828005e8a2680cfab8f9cea232ed24c8f27a
Damien Milon <milon.damien@gmail.com>
2020-04-10 13:05:07 +0200

    Remove binding

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from restaurant_routes import Restaurants, RestaurantMember
 import os
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DATABASE_URL", "sqlite:///data.db")
 api = Api(app)
 @app.before_first_request
 def create_tables():
@@ -16,4 +16,4 @@ api.add_resource(RestaurantMember, "/restaurants/<id>")
 
 db.init_app(app)
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    # app.run(host="0.0.0.0", port=5000)
